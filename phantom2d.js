ecccb392a77155f0bc664a2d9363573a30e58542
BeanTheAlien <ben.allen.goldstein@gmail.com>
2025-10-02 13:14:14 -0500

    Updated StaticObject and PhysicsObject Both StaticObject and PhysicsObject extend SceneObject

--- phantom2d.js
+++ phantom2d.js
@@ -18,7 +18,7 @@ class Scene {
 }
 class SceneObject {
   constructor(expects, objname, settings) {
-    if(!expect(settings, ["name", "shape", "collide", ...expects])) throw new Error("Missing key(s) in " + objname + " settings.");
+    if(!expect(settings, ["name", "shape", "collide", ...expects])) throw new Error("Missing key(s) in " + objname + " object settings.");
     this.name = settings.name;
     this.shape = settings.shape;
     this.collide = settings.collide ?? (() => {});
@@ -28,24 +28,26 @@ class SceneObject {
     this.height = settings.height ?? 0;
   }
 }
-class StaticObject {
-  if(!expect(settings, ))
-}
-class PhysicsObject {
+class StaticObject extends SceneObject {
   constructor(settings) {
-    if(!expect(settings, ["name", "shape", "strength", "collide"])) throw new Error("Missing key(s) in physics object settings.");
-    this.name = settings.name;
-    this.shape = settings.shape;
+    super([], "static", settings);
+  }
+  update() {}
+}
+class PhysicsObject extends SceneObject {
+  constructor(settings) {
+    super(["strength"], "physics", settings);
     this.strength = settings.strength;
     this.gravspd = 0;
-    this.collide = settings.collide ?? (() => {});
-    this.pos = { x: settings.px ?? 0, y: settings.py ?? 0 };
-    this.rot = { x: settings.rx ?? 0, y: settings.ry ?? 0 };
-    this.width = settings.width ?? 0;
-    this.height = settings.height ?? 0;
   }
   update() {
     this.gravspd += this.strength;
     this.pos.y += this.gravspd;
+  }
+}
+class Vector {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
   }
 }
