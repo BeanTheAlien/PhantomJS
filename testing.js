c9e98c1e1284c3d96f3fedc44182b24390f728f4
BeanTheAlien <ben.allen.goldstein@gmail.com>
2025-10-03 10:20:09 -0500

    Updated MO ex var name

--- testing.js
+++ testing.js
@@ -13,7 +13,7 @@ const statics = [
 scene.add(...statics);
 // note that MovingObject is experimental and I haven't done testing on it yet
 // Use with caution
-const mymo = new phantom.MovingObject({
+const movingObject = new phantom.MovingObject({
     id: "mymo",
     shape: "rect",
     dirX: 1, dirY: 0,
