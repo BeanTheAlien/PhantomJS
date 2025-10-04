4712a081b3c4861988a5ce242bdee6a2f6a05f36
BeanTheAlien <ben.allen.goldstein@gmail.com>
2025-10-04 17:37:50 -0500

    fixed expect, findMissing

--- phantom.js
+++ phantom.js
@@ -1,15 +1,11 @@
 // Phantom.js v0.0.0
 
 function expect(obj, keys) {
-  return Object.keys(obj).every(k => keys.includes(k));
+  return keys.every(k => Object.prototype.hasOwnProperty.call(obj, k));
 }
 
 function findMissing(input, expected) {
-  let missing = [];
-  for(const key of expected) {
-    if(!Object.keys(input).includes(key)) missing.push(key);
-  }
-  return missing;
+  return expected.filter(k => !Object.prototype.hasOwnProperty.call(input, k));
 }
 
 export { expect, findMissing };
