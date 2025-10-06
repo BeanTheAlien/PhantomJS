59c9b0cc0e85b8bec2019ff9f901ca22e1d71e43
BeanTheAlien <ben.allen.goldstein@gmail.com>
2025-10-06 10:01:40 -0500

    Added random

--- phantom.js
+++ phantom.js
@@ -8,4 +8,26 @@ function findMissing(input, expected) {
   return expected.filter(k => !Object.prototype.hasOwnProperty.call(input, k));
 }
 
-export { expect, findMissing };
+function random(a = null, b = null) {
+    if(a == null && b == null) {
+        return Math.floor(Math.random() * 101);
+    } else if(a != null && b == null) {
+        return Math.floor(Math.random() * a);
+    } else if(a != null && b != null) {
+        let min;
+        let max;
+        if(a > b) {
+            min = Math.ceil(b);
+            max = Math.floor(a);
+            return Math.floor(Math.random() * (max - min)) + min;
+        } else if(a < b) {
+            min = Math.ceil(a);
+            max = Math.floor(b);
+            return Math.floor(Math.random() * (max - min)) + min;
+        } else {
+            return Math.floor(Math.random() * a);
+        }
+    }
+}
+
+export { expect, findMissing, random };
