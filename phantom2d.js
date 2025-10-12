47976ea53e38304df172e8bb6fa1effd8598c431
BeanTheAlien <ben.allen.goldstein@gmail.com>
2025-10-12 13:54:21 -0500

    Updated Phantom2DEntity._id => id

--- phantom2d.js
+++ phantom2d.js
@@ -6,7 +6,7 @@ class Phantom2DEntity {
     // id, shape and color are all base properties
     const base = ["id", "shape", "color"];
     if(!expect(settings, [...base, ...expects])) throw new Error(`Missing keys(s) in ${objname} settings. (missing: ${findMissing(settings, [...base, ...expects]).join(", ")})`);
-    this._id = settings.id;
+    this.id = settings.id;
     this.shape = settings.shape;
     this.color = settings.color;
     this.collide = settings.collide ?? (() => {});
