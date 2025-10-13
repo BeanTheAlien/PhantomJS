70ede074234e95349e7ae4dc870be2b078fe1123
BeanTheAlien <ben.allen.goldstein@gmail.com>
2025-10-13 13:49:42 -0500

    Removed ctxmenu temporarily to continue more important development

--- visualeditor/visualeditor.js
+++ visualeditor/visualeditor.js
@@ -1,26 +1,26 @@
 import * as phantom from "./../phantom2d.js";
 
-const actionList = document.createElement("div");
-actionList.className = "ve-cm";
-document.body.appendChild(actionList);
+// const actionList = document.createElement("div");
+// actionList.className = "ve-cm";
+// document.body.appendChild(actionList);
 
 const projectDiv = document.createElement("div");
 projectDiv.className = "ve-pdiv";
-projectDiv.addEventListener("contextmenu", ActionList);
+// projectDiv.addEventListener("contextmenu", ActionList);
 document.body.appendChild(projectDiv);
 var project = [];
 
-function ActionList(e) {
-    e.preventDefault();
-    actionList.style.display = "block";
-    actionList.style.left = `${e.pageX - 10}px`;
-    actionList.style.top = `${e.pageY - 10}px`;
-}
+// function ActionList(e) {
+//     e.preventDefault();
+//     actionList.style.display = "block";
+//     actionList.style.left = `${e.pageX - 10}px`;
+//     actionList.style.top = `${e.pageY - 10}px`;
+// }
 
-document.addEventListener("click", (e) => {
-    if(e.target != actionList) {
-        actionList.style.display = "none";
-        actionList.style.left = "";
-        actionList.style.top = "";
-    }
-});
+// document.addEventListener("click", (e) => {
+//     if(e.target != actionList) {
+//         actionList.style.display = "none";
+//         actionList.style.left = "";
+//         actionList.style.top = "";
+//     }
+// });
