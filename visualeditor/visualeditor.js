903c7dc94ed8c568a779d2eac0bc61cb7817c1f6
BeanTheAlien <ben.allen.goldstein@gmail.com>
2025-10-13 11:35:25 -0500

    Updated visual editor stuff

---
+++ visualeditor/visualeditor.js
@@ -0,0 +1,6 @@
+import * as phantom from "../phantom2d";
+
+const projectDiv = document.createElement("div");
+projectDiv.className = "ve-pdiv";
+document.body.appendChild(projectDiv);
+var project = [];
