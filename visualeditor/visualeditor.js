eab522bd7b69532f33f475b6f1fc00884f9cd556
BeanTheAlien <ben.allen.goldstein@gmail.com>
2025-10-13 13:41:47 -0500

    Fixed CSS / updated JS

--- visualeditor/visualeditor.js
+++ visualeditor/visualeditor.js
@@ -1,6 +1,15 @@
-import * as phantom from "../phantom2d";
+import * as phantom from "./../phantom2d.js";
+
+const actionList = document.createElement("div");
+actionList.className = "ve-cm";
+document.body.appendChild(actionList);
 
 const projectDiv = document.createElement("div");
 projectDiv.className = "ve-pdiv";
+// projectDiv.addEventListener("contextmenu", ActionList);
 document.body.appendChild(projectDiv);
 var project = [];
+
+// function ActionList(e) {
+//     e.preventDefault();
+// }
