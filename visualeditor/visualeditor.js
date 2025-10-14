02419987549848ed3b563bba6b3350245a7b479c
BeanTheAlien <ben.allen.goldstein@gmail.com>
2025-10-13 20:32:25 -0500

    Added Phantom2DEntity

--- visualeditor/visualeditor.js
+++ visualeditor/visualeditor.js
@@ -24,3 +24,28 @@ var project = [];
 //         actionList.style.top = "";
 //     }
 // });
+
+function Phantom2DEntity() {
+    return `<div>
+        <label for="id_input">Enter ID:</label>
+        <input type="text" id="id_input" placeholder="Enter id" name="id_input">
+        <label for="shape_input">Select shape:</label>
+        <select id="shape_input" name="shape_input">
+            <option value="rect">Rectangle</option>
+        </select>
+        <label for="colour_input">Select colour:</label>
+        <input type="color" id="colour_input" name="colour_input">
+        <label for="x_input">Enter x-position:</label>
+        <input type="number" id="x_input" name="x_input">
+        <label for="y_input">Enter y-position:</label>
+        <input type="number" id="y_input" name="y_input">
+        <label for="rot_input">Enter rotation:</label>
+        <input type="number" id="rot_input" name="rot_input" min="0" max="360">
+        <label for="width_input">Enter width:</label>
+        <input type="number" id="width_input" name="width_input">
+        <label for="height_input">Enter height:</label>
+        <input type="number" id="height_input" name="height_input">
+    </div>`;
+}
+
+// collide, custom props
