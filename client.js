ad8e21a68b0eb29945b993f109c1d6918e4585d3
BeanTheAlien <ben.allen.goldstein@gmail.com>
2025-11-11 07:10:49 -0600

    Added player and msg.type.move

--- client.js
+++ client.js
@@ -11,13 +11,40 @@ function game() {
 }
 game();
 
+const player = new p2d.PlayableCharacter({
+    id: "ws_player", shape: "rect", color: "blue",
+    width: 10, height: 30, x: 50, y: 50, strength: 0,
+    binds: {
+        "w": () => {
+            player.moveY(-1);
+            ws.send(player.movePayload(-1, 1));
+        },
+        "a": () => {
+            player.moveX(-1);
+            ws.send(player.movePayload(-1, 0));
+        },
+        "s": () => {
+            player.moveY(1);
+            ws.send(player.movePayload(1, 1));
+        },
+        "d": () => {
+            player.move(1);
+            ws.send(player.movePayload(1, 0));
+        }
+    }, custom: {
+        movePayload: (d, a) => JSON.stringify({ type: "move", entity: player, distance: d, axis: a })
+    }
+});
+
 ws.onopen = () => {
     console.log("Connected to server");
 }
 
 ws.onmessage = (e) => {
     const msg = JSON.parse(e.data);
-    // do something based on message
+    if(msg.type == "move") {
+        scene.getById(msg.entity.id).move(msg.distance, msg.axis);
+    }
 }
 
 ws.onclose = () => {
