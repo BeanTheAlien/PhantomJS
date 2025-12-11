d3d1b76bc8ca4ef52b7bd204edd0599c2072ccb1
BeanTheAlien <ben.allen.goldstein@gmail.com>
2025-12-11 07:45:08 -0600

    Added more barrels

--- mario/mario.js
+++ mario/mario.js
@@ -5,13 +5,13 @@ const canvas = document.getElementById("p2d-canvas");
 const scene = new phantom.Scene(canvas, 500, 500, "100vw", "100vh");
 const mario = new phantom.PlayableCharacter({
     id: "mario", shape: "rect", color: "#be1414ff",
-    width: 5, height: 10, x: 10, y: 10, strength: 0.03,
+    width: 5, height: 10, x: 10, y: 10, strength: 0.04,
     binds: {
         w: () => {
             if(flure.some(f => phantom.isColliding(mario, f))) mario.jump(1.5);
         },
-        a: () => mario.moveX(-1),
-        d: () => mario.moveX(1)
+        a: () => mario.moveX(-mario.spd),
+        d: () => mario.moveX(mario.spd)
     },
     collide: (c) => {
         if(c instanceof Barrel) {
@@ -23,8 +23,9 @@ const mario = new phantom.PlayableCharacter({
             phantom.resolveYCol(mario, flure.find(f => phantom.isColliding(mario, f)));
             return;
         }
-        mario.strength = 0.03;
-    }
+        mario.strength = 0.04
+    },
+    custom: { spd: 1.5 }
 });
 class Flur extends phantom.WallObject {
     constructor(x, y, width) {
@@ -49,13 +50,13 @@ class Barrel extends phantom.Phantom2DEntity {
     update() {
         if(this.level % 2 == 0) {
             this.moveX(this.spd);
-            if(this.x >= scene.width * 0.75) {
+            if(this.x >= scene.width * 0.85) {
                 this.level++;
                 this.moveY(10);
             }
         } else {
             this.moveX(-this.spd);
-            if(this.x <= scene.width * 0.25) {
+            if(this.x <= scene.width * 0.15) {
                 this.level++;
                 this.moveY(10);
             }
@@ -63,7 +64,13 @@ class Barrel extends phantom.Phantom2DEntity {
     }
 }
 const b = new Barrel(5);
-scene.add(b);
+const b2 = new Barrel(4);
+const b3 = new Barrel(3);
+const b4 = new Barrel(2);
+const b5 = new Barrel(1);
+const b6 = new Barrel(6);
+const b7 = new Barrel(7);
+[b,b2,b3,b4,b5,b6,b7].forEach(bl => scene.add(bl));
 
 var gameOver = 0;
 function render() {
