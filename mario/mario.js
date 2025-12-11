6c8ed67f81a1fedfa1a803797348fbea61082990
BeanTheAlien <ben.allen.goldstein@gmail.com>
2025-12-11 08:29:34 -0600

    Minor changes

--- mario/mario.js
+++ mario/mario.js
@@ -32,19 +32,22 @@ class Flur extends phantom.WallObject {
         super({ id: "flur", shape: "rect", color: "#680f0fff", x, y, width, height: 10 });
     }
 }
+function wp(p) {
+    return scene.width * p;
+}
 scene.add(mario);
-const flur = new Flur(0, 50, 0.75 * scene.width);
-scene.add(flur);
-const flur2 = new Flur(scene.width * 0.25, 100, 0.75 * scene.width);
-scene.add(flur2);
+const flur = new Flur(0, 50, wp(0.75));
+const flur2 = new Flur(wp(0.25), 100, wp(0.75));
+const flur3 = new Flur(0, scene.height, wp(0.75));
+scene.add(flur, flur2, flur3);
 const flure = [flur, flur2];
 
 class Barrel extends phantom.Phantom2DEntity {
-    constructor(spd) {
+    constructor(y, spd = 5) {
         super([], "", {
             id: "barrel", shape: "rect", color: "#cf8112ff", collide: (c) => {
                 if(c.id == mario.id) gameOver = 1;
-            }, x: 0, y: 0, width: 15, height: 10, custom: { spd, level: 0 }
+            }, x: 0, y, width: 15, height: 10, custom: { spd, level: 0 }
         });
     }
     update() {
@@ -52,25 +55,19 @@ class Barrel extends phantom.Phantom2DEntity {
             this.moveX(this.spd);
             if(this.x >= scene.width * 0.85) {
                 this.level++;
-                this.moveY(10);
+                this.lerpY(this.y + 30);
             }
         } else {
             this.moveX(-this.spd);
             if(this.x <= scene.width * 0.15) {
                 this.level++;
-                this.moveY(10);
+                this.lerpY(this.y + 30);
             }
         }
     }
 }
-const b = new Barrel(5);
-const b2 = new Barrel(4);
-const b3 = new Barrel(3);
-const b4 = new Barrel(2);
-const b5 = new Barrel(1);
-const b6 = new Barrel(6);
-const b7 = new Barrel(7);
-[b,b2,b3,b4,b5,b6,b7].forEach(bl => scene.add(bl));
+const b = new Barrel(40);
+scene.add(b);
 
 var gameOver = 0;
 function render() {
