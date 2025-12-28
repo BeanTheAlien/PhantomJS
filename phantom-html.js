558102f9a144f82129cafee1caa5061e4629fec0
BeanTheAlien <ben.allen.goldstein@gmail.com>
2025-12-28 13:47:53 -0600

    Update phantom-html.js

--- phantom-html.js
+++ phantom-html.js
@@ -1,8 +1,18 @@
+import { expect, findMissing, random, is, wait, getRemoteImg, getRemoteAudio, download, downloadJSON } from "./phantom.js";
 
 class PhantomElement {
+    #root;
     constructor(expects, objname, settings) {
-        // id, shape and color are all base properties
-        const base = ["id", "shape", "color"];
-        if(!expect(settings, [...base, ...expects])) throw new Error(`Missing keys(s) in ${objname} settings. (missing: ${findMissing(settings, [...base, ...expects]).join(", ")})`);
+        //if(!expect(settings, [...base, ...expects])) throw new Error(`Missing keys(s) in ${objname} settings. (missing: ${findMissing(settings, [...base, ...expects]).join(", ")})`);
+        this.#root = document.createElement("div");
+        this.left = settings.left ?? 0;
+        this.top = settings.top ?? 0;
+        this.right = settings.right ?? 0;
+        this.bottom = settings.bottom ?? 0;
+    }
+    #genStyles() {
+        Object.assign(this.#root.style, {
+            left: this.left
+        });
     }
 }
