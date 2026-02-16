16f59b9188e654b48a207d31aa04fc78b2fa74cd
BeanTheAlien <ben.allen.goldstein@gmail.com>
2026-02-16 15:42:13 -0600

    Started making ts p2d

---
+++ phantom2d.ts
@@ -0,0 +1,15 @@
+interface Phantom2dOptions {
+    collide?: Function;
+    x?: number;
+    y?: number;
+    rot?: number;
+    width?: number;
+    height?: number;
+    custom?: { any: any };
+}
+
+class Phantom2dEntity {
+    constructor(expects: string[], objname: string, settings: Phantom2dOptions) {
+        //
+    }
+}
