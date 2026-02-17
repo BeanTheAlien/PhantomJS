b47a79a715a460a552e8443a3952ae4d7f543200
BeanTheAlien <ben.allen.goldstein@gmail.com>
2026-02-17 07:14:31 -0600

    Updates for TS rework

--- phantom2d.ts
+++ phantom2d.ts
@@ -1,4 +1,6 @@
 type Custom = { any?: any };
+type Axis = "x" | "y" | 0 | 1;
+const NoFunc: Function = (() => {});
 interface Phantom2dOptions {
     collide?: Function;
     x?: number;
@@ -7,15 +9,24 @@ interface Phantom2dOptions {
     width?: number;
     height?: number;
     custom?: Custom;
+    upd?: Function;
+}
+interface StaticObjectOptions extends Phantom2dOptions {
+    shape: "rect" | "circle";
+    color: string;
+}
+interface PhysicsObjectOptions extends Phantom2dOptions {
+    strength: number;
 }
 
 class Phantom2dEntity {
-    collide: Function;
+    collide: Function; upd: Function;
     x: number; y: number;
     rot: number;
     width: number; height: number;
     constructor(opts: Phantom2dOptions) {
-        this.collide = opts.collide ?? (() => {});
+        this.collide = opts.collide ?? NoFunc;
+        this.upd = opts.upd ?? NoFunc;
         this.x = opts.x ?? 0;
         this.y = opts.y ?? 0;
         this.rot = opts.rot ?? 0;
@@ -39,6 +50,71 @@ class Phantom2dEntity {
     }
     setHeight(h: number) {
         this.height = h;
+    }
+    getFVec(): Vector {
+        const dx = Math.cos(this.rot);
+        const dy = Math.sin(this.rot);
+        return new Vector(dx, dy);
+    }
+    move(dist: number, axis: Axis) {
+        if(axis == "x" || axis == 0) this.x += dist;
+        else if(axis == "y" || axis == 1) this.y += dist;
+    }
+    moveX(dist: number) {
+        this.move(dist, "x");
+    }
+    moveY(dist: number) {
+        this.move(dist, "y");
+    }
+    clampPos(min: number, max: number, axis: Axis) {
+        const clamp = (n: number): number => Math.min(Math.max(n, min), max);
+        if(axis == "x" || axis == 0) this.x = clamp(this.x);
+        else if(axis == "y" || axis == 1) this.y = clamp(this.y);
+    }
+    clampPosX(min: number, max: number) {
+        this.clampPos(min, max, "x");
+    }
+    clampPosY(min: number, max: number) {
+        this.clampPos(min, max, "y");
+    }
+    getPos(): Vector {
+        return new Vector(this.x, this.y);
+    }
+    getCenter(): Vector {
+        return new Vector(this.x + this.width / 2, this.y + this.height / 2);
+    }
+    getPosX(): number {
+        return this.x;
+    }
+    getPosY(): number {
+        return this.y;
+    }
+    setPosX(x: number) {
+        this.x = x;
+    }
+    setPosY(y: number) {
+        this.y = y;
+    }
+    update() {
+        this.upd();
+    }
+}
+class StaticObject extends Phantom2dEntity {
+    constructor(opts: StaticObjectOptions) {
+        super(opts);
+    }
+}
+class PhysicsObject extends Phantom2dEntity {
+    strength: number; gravspd: number;
+    constructor(opts: PhysicsObjectOptions) {
+        super(opts);
+        this.strength = opts.strength;
+        this.gravspd = 0;
+    }
+    update() {
+        this.gravspd += this.strength;
+        this.y += this.gravspd;
+        super.update();
     }
 }
 class Vector {
