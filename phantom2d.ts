80fe73041afedd66b959ed5aece239f2d19d5365
BeanTheAlien <ben.allen.goldstein@gmail.com>
2026-02-16 20:02:40 -0600

    Updated

--- phantom2d.ts
+++ phantom2d.ts
@@ -1,3 +1,4 @@
+type Custom = { any?: any };
 interface Phantom2dOptions {
     collide?: Function;
     x?: number;
@@ -5,11 +6,45 @@ interface Phantom2dOptions {
     rot?: number;
     width?: number;
     height?: number;
-    custom?: { any: any };
+    custom?: Custom;
 }
 
 class Phantom2dEntity {
-    constructor(expects: string[], objname: string, settings: Phantom2dOptions) {
-        //
+    collide: Function;
+    x: number; y: number;
+    rot: number;
+    width: number; height: number;
+    constructor(opts: Phantom2dOptions) {
+        this.collide = opts.collide ?? (() => {});
+        this.x = opts.x ?? 0;
+        this.y = opts.y ?? 0;
+        this.rot = opts.rot ?? 0;
+        this.width = opts.width ?? 0;
+        this.height = opts.height ?? 0;
+    }
+    setPos(x: number | Vector, y?: number) {
+        if (typeof x == "number" && typeof y == "number") {
+            this.x = x;
+            this.y = y;
+        } else if (x instanceof Vector) {
+            this.x = x.x;
+            this.y = x.y;
+        }
+    }
+    setRot(rad: number) {
+        this.rot = rad;
+    }
+    setWidth(w: number) {
+        this.width = w;
+    }
+    setHeight(h: number) {
+        this.height = h;
+    }
+}
+class Vector {
+    x: number; y: number;
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
     }
 }
